3db19d2c5d55d5dfc79c11386b1b982ec8a73c07
Li Tan <litan1106@gmail.com>
2024-01-19 15:03:26 -0800

    wip

--- scripts/features/mongodb.sh
+++ scripts/features/mongodb.sh
@@ -21,11 +21,11 @@ ARCH=$(arch)
 touch /home/$WSL_USER_NAME/.homestead-features/mongodb
 chown -Rf $WSL_USER_NAME:$WSL_USER_GROUP /home/$WSL_USER_NAME/.homestead-features
 
-curl -fsSL https://www.mongodb.org/static/pgp/server-6.0.asc | sudo gpg --dearmor -o /etc/apt/keyrings/mongodb.gpg
+curl -fsSL https://www.mongodb.org/static/pgp/server-$1.asc | sudo gpg --dearmor -o /etc/apt/keyrings/mongodb.gpg
 if [[ "$ARCH" == "aarch64" ]]; then
-  echo "deb [signed-by=/etc/apt/keyrings/mongodb.gpg arch=arm64] https://repo.mongodb.org/apt/ubuntu $(lsb_release -cs)/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
+  echo "deb [signed-by=/etc/apt/keyrings/mongodb.gpg arch=arm64] https://repo.mongodb.org/apt/ubuntu $(lsb_release -cs)/mongodb-org/$1 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-$1.list
 else
-  echo "deb [signed-by=/etc/apt/keyrings/mongodb.gpg arch=amd64] https://repo.mongodb.org/apt/ubuntu $(lsb_release -cs)/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
+  echo "deb [signed-by=/etc/apt/keyrings/mongodb.gpg arch=amd64] https://repo.mongodb.org/apt/ubuntu $(lsb_release -cs)/mongodb-org/$1 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-$1.list
 fi
 
 sudo apt-get update
