5673f459d3c30a0f78768a55af70aa1bf35ef543
Li Tan <litan1106@gmail.com>
2024-08-05 11:10:57 -0700

    removed duplicate go path

--- scripts/features/golang.sh
+++ scripts/features/golang.sh
@@ -31,7 +31,6 @@ else
 fi
 
 tar -C /usr/local -xzf golang.tar.gz go
-printf "\nPATH=\"/usr/local/go/bin:\$PATH\"\n" | tee -a /home/vagrant/.profile
 rm -rf golang.tar.gz
 
 echo 'export GOROOT=/usr/local/go
