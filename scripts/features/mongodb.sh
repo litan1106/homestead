44361c27f7cfa3303ed34181a5b512cd37a6629b
Li Tan <litan1106@gmail.com>
2025-04-11 10:59:24 -0700

    updated mongo pecl

--- scripts/features/mongodb.sh
+++ scripts/features/mongodb.sh
@@ -38,17 +38,11 @@ sudo sed -i "s/bindIp: .*/bindIp: 0.0.0.0/" /etc/mongod.conf
 sudo systemctl enable mongod
 sudo systemctl start mongod
 
-sudo rm -rf /tmp/mongo-php-driver /usr/src/mongo-php-driver
-git clone -c advice.detachedHead=false -q -b "$2" --single-branch https://github.com/mongodb/mongo-php-driver.git /tmp/mongo-php-driver
-sudo mv /tmp/mongo-php-driver /usr/src/mongo-php-driver
-cd /usr/src/mongo-php-driver
-git submodule -q update --init
+# Update PECL Channel
+sudo pecl channel-update pecl.php.net
+# Install mongo
+sudo pecl install mongo-$2
 
-phpize"$3"
-./configure --with-php-config=/usr/bin/php-config"$3" > /dev/null
-make clean > /dev/null
-make >/dev/null 2>&1
-sudo make install
 sudo bash -c "echo 'extension=mongodb.so' > /etc/php/$3/mods-available/mongo.ini"
 sudo ln -s /etc/php/"$3"/mods-available/mongo.ini /etc/php/"$3"/cli/conf.d/20-mongo.ini
 sudo ln -s /etc/php/"$3"/mods-available/mongo.ini /etc/php/"$3"/fpm/conf.d/20-mongo.ini
